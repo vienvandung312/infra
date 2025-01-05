653321e64e1068a16e69d60b61095f83258ec069
Ryan Vien <ryanvien@MacBookAir.lan>
2025-01-06 01:32:36 +0700

    (fix): put terraform configs into the main.tf"

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "main-terraform-state-79810"
+    key    = "global/terraform.tfstate"
+    region = "ap-southeast-1"
+    dynamodb_table = "terraform-state-lock"
+  }
+}
+
 provider "aws" {
     region = "ap-southeast-1"
 }
--- terraform/main.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "main-terraform-state-79810"
-    key    = "global/terraform.tfstate"
-    region = "ap-southeast-1"
-    dynamodb_table = "terraform-state-lock"
-  }
-}
