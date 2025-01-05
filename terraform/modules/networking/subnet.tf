56d6c4ce31b22e163fd5b3a149211efb836b039e
Ryan Vien <ryanvien@MacBookAir.lan>
2025-01-06 03:59:43 +0700

    (feat): HA subnet to multiple AZs

--- terraform/modules/networking/subnet.tf
+++ terraform/modules/networking/subnet.tf
@@ -1,13 +1,17 @@
+data "aws_availability_zones" "available" {}
+
 resource "aws_subnet" "private_subnets" {
-    count = 1 
+    count = 2
     vpc_id = local.vpc_id
     cidr_block = cidrsubnet(var.vpc_cidr_block, 8, count.index)
+    availability_zone = element(data.aws_availability_zones.available.names, count.index)
 }
 
 resource "aws_subnet" "public_subnets" {
-    count = 1
+    count = 2
     vpc_id = local.vpc_id
     cidr_block = cidrsubnet(var.vpc_cidr_block, 8, count.index + 1)
+    availability_zone = element(data.aws_availability_zones.available.names, count.index)
 }
 
 output "private_subnets_ids" {
