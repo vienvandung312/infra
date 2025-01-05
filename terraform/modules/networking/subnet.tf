1ec8e51f42b15aa9398ae08557e2c11a86cb6b05
Ryan Vien <ryanvien@MacBookAir.lan>
2025-01-06 04:05:13 +0700

    (fix): ensure subnets availability

--- terraform/modules/networking/subnet.tf
+++ terraform/modules/networking/subnet.tf
@@ -5,13 +5,20 @@ resource "aws_subnet" "private_subnets" {
     vpc_id = local.vpc_id
     cidr_block = cidrsubnet(var.vpc_cidr_block, 8, count.index)
     availability_zone = element(data.aws_availability_zones.available.names, count.index)
+    tags = {
+        Name = "Private Subnet ${count.index + 1}"
+    }
 }
 
 resource "aws_subnet" "public_subnets" {
     count = 2
     vpc_id = local.vpc_id
-    cidr_block = cidrsubnet(var.vpc_cidr_block, 8, count.index + aws_subnet.private_subnets.count)
+    cidr_block = cidrsubnet(var.vpc_cidr_block, 8, count.index + length(aws_subnet.private_subnets))
     availability_zone = element(data.aws_availability_zones.available.names, count.index)
+    map_public_ip_on_launch = true
+    tags = {
+        Name = "Public Subnet ${count.index + 1}"
+    }
 }
 
 output "private_subnets_ids" {
