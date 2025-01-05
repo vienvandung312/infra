9f8e2493f65c3b3a37945aba76c6e63852707645
Ryan Vien <ryanvien@MacBookAir.lan>
2025-01-06 03:20:43 +0700

    (feat): trigger eks module

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -16,4 +16,11 @@ module "networking" {
     vpc_cidr_block = "10.0.0.0/16"
 }
 
-
+module "eks" {
+    source = "../../modules/eks"
+    cluster_name = "main-eks-cluster"
+    subnets = module.networking.private_subnets_ids
+    vpc_id = module.networking.vpc_id
+    desired_capacity = 2
+    instance_type = "t3.medium"
+}
