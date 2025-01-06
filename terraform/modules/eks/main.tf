3cdac7c4168e8b730a99ba80d7ba4ecc35326d22
Ryan Vien <ryanvien@MacBookAir.lan>
2025-01-07 01:35:41 +0700

    (feat): adding kafka cluster in and kubectl into k8s

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -43,3 +43,14 @@ module "eks" {
     }
 }
 
+output "eks_cluster_name" {
+    value = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+    value = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_arn" {
+    value = module.eks.cluster_arn
+}
