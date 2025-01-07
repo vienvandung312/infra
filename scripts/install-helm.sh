125ca90a250b14b95b6770e43f7eed786b26f32e
Ryan Vien <ryanvien@MacBookAir.lan>
2025-01-08 02:45:20 +0700

    (fix): Change k3s to minikube

--- scripts/install-helm.sh
+++ scripts/install-helm.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 
-# Install K3s
-curl -sfL https://get.k3s.io | sh -
-
 # Install Helm
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-
-# Add Bitnami repository
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo update
-
-# Create namespace
-kubectl create namespace kafka
-
-# Deploy Kafka in KRaft mode
-helm install kafka bitnami/kafka -f ./kafka/values.yaml --namespace kafka
---
+++ scripts/install-minikube.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install Minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+# Start Minikube
+minikube start --driver=docker
