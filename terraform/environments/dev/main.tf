310d333d99d9429fef5ef0cd3cefbfc6a05b6695
Ryan Vien <ryanvien@MacBookAir.lan>
2025-01-08 00:13:01 +0700

    (feat): optimized ssh authentication process

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -15,9 +15,14 @@ variable "key_pair_name" {
     type = string
 }
 
+variable "public_key" {
+    type = string
+}   
+
 module "vm" {
     source = "../../modules/vm"
     key_pair_name = var.key_pair_name
+    public_key = var.public_key
 }
 
 output "instance_ip" {
--- terraform/modules/vm/lightsail.tf
+++ terraform/modules/vm/lightsail.tf
@@ -4,6 +4,16 @@ variable "key_pair_name" {
     type = string
 }
 
+variable "public_key" {
+    type = string
+}
+
+resource "aws_lightsail_key_pair" "key_pair" {
+    name = var.key_pair_name
+    public_key = var.public_key
+  
+}
+
 resource "aws_lightsail_instance" "k3s_node" {
     name = "k3s-lightsail"
     availability_zone = data.aws_availability_zones.available.names[0]
