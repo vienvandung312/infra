be3d72029538d67db6d259d90532f07f12711526
Ryan Vien <ryanvien@MacBookAir.lan>
2025-01-16 02:13:22 +0700

    fix: fix the script generating ansible inventory

--- scripts/generate_inventory.sh
+++ scripts/generate_inventory.sh
@@ -7,7 +7,7 @@ for ip in $NGINX_IPS; do
 done
 
 echo "[k8s_nodes]" >> ./ansible/inventory.ini
-K8S_IPS=$(terraform -chdir=$TF_PATH output -json k8s_nodes_public_ips | jq -r '.[]')
+K8S_IPS=$(terraform -chdir=$TF_PATH output -json k8s_node_public_ips | jq -r '.[]')
 for ip in $K8S_IPS; do
   echo "$ip ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa" >> ./ansible/inventory.ini
 done
